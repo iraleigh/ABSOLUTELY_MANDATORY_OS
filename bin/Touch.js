2ad5616da6b25286365d673f935c9232bacb8dc0
Matt <mwishoff@yahoo.com>
2016-03-30 17:55:09 -0700

    Do not merge this. Still testing.

--- bin/Touch.js
+++ bin/Touch.js
@@ -4,18 +4,48 @@
 var Touch = function(counter){
 
     var arg = this.args;
+    var doesntExist;
+
+    console.log(arg);
+    console.log(OS.FS.open(arg));
 
     switch(counter)
     {
         case 0:
-            OS.FS.create(CLI.currentInput, arg); //CLI.currentInput is the file name after that touch command.
-            console.log(arg + "File created");
+            if(OS.FS.open(arg) == undefined)
+            {
+                doesntExist = true;
+            }
+            else
+            {
+                doesntExist = false;
+            }
+            break;
+
+        case 1:
+            if(doesntExist == true)
+            {
+                OS.FS.create(CLI.currentInput, arg); //CLI.currentInput is the file name after that touch command.
+                console.log(arg + " File created");
+            }
+            else
+            {
+                arg.setDate(new Date());
+                console.log("Updated date for file " + arg);
+            }
+            break;
+
+        case 2:
+            if(doesntExist == false)
+            {
+                OS.FS.close(arg);
+            }
             break;
 
         default:
             this.state = "Stop";
     }
-    
+
 
     //http://www.linfo.org/touch.html
     //Syntax: touch file_name
