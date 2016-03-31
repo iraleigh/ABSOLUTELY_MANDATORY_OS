9f73db7cee9aebe3a27c47356693540133939d59
Matt <mwishoff@yahoo.com>
2016-03-31 12:24:50 -0700

    Code works. -Creates a file if the file does note exist. -If the file exists it will update the date object with the current date.

--- bin/Touch.js
+++ bin/Touch.js
@@ -3,49 +3,44 @@
  */
 var Touch = function(counter){
 
-    var arg = this.args;
-    var doesntExist;
-
-    console.log(arg);
-    console.log(OS.FS.open(arg));
-
     switch(counter)
     {
         case 0:
-            if(OS.FS.open(arg) == undefined)
+            this.var.fileName = this.args;
+
+            if(OS.FS.open(this.fileName) == undefined)
             {
-                doesntExist = true;
+                this.var.doesntExist = true;
+                //console.log("doesntExist == TRUE~~~~~~~~~~~~~")
             }
             else
             {
-                doesntExist = false;
+                this.var.doesntExist = false;
+                //console.log("doesntExist == FALSE~~~~~~~~~~~~~")
             }
             break;
 
         case 1:
-            if(doesntExist == true)
+
+            if(this.var.doesntExist == true)
             {
-                OS.FS.create(CLI.currentInput, arg); //CLI.currentInput is the file name after that touch command.
-                console.log(arg + " File created");
+                OS.FS.create(this.var.fileName, ""); //CLI.currentInput is the file name after that touch command.
+                console.log(this.var.fileName + " has been created");
             }
             else
             {
-                arg.setDate(new Date());
-                console.log("Updated date for file " + arg);
+                this.var.fileName.setDate(new Date());
+                console.log("Updated date for file - " + this.var.fileName);
             }
             break;
 
         case 2:
-            if(doesntExist == false)
-            {
-                OS.FS.close(arg);
-            }
+                OS.FS.close(this.var.fileName);
             break;
 
         default:
             this.state = "Stop";
     }
-
 
     //http://www.linfo.org/touch.html
     //Syntax: touch file_name
