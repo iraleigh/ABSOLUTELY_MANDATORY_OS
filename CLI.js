3061cc55f001bd1dbff7fe2fdee9de4c19e74973
Alex Brown <abrown@mail.sfsu.edu>
2016-04-12 22:53:37 -0700

    Added CLI blinking cursor, up/down hisotyr, exit command

--- CLI.js
+++ CLI.js
@@ -4,44 +4,52 @@ var CLI = {
     textHeight: 0,
     STDIn: "",
     STDOut: "",
-    STDErr: "",
+    cursor: "_",
     status: {
         OK: 0,
         BAD_COMMAND: 1
     },
-    getSTDIn: function(){
+    commandHistory: [],
+    commandPosition: 0,
+    getSTDIn: function () {
         return this.STDIn.trim();
     }
 }
 
 var container;
-window.onload = function () { 
-
+window.onload = function () {
     Processes.generateListOfProcesses();
     container = window.document.getElementById('container');
 
     addDummyFiles();
     console.log(Directory.Files);
     //ABSOLUTELY MANDATORY OS -- AMOS
+    setInterval(cursor, 500);
+    CLI.commandHistory = new Array();
+    CLI.commandHistory.push("");
     container.innerHTML = CLI.oldInput;
     CLI.textHeight = document.getElementById('container').offsetHeight;
     console.log(CLI.textHeight);
     CLI.oldInput += "<br /> \\> ";
-    container.innerHTML = CLI.oldInput;
+    container.innerHTML = CLI.oldInput + CLI.cursor;
 
 }
 
 document.onkeypress = function (evt) {
     evt = evt || window.event;
     if (evt.charCode == 13) { // On enter
+        if (CLI.currentInput == "exit") //This can be hard coded...
+            window.close(); 
         //Grab the function here
+        CLI.commandPosition = 0;
+        CLI.commandHistory.splice(1, 0, CLI.currentInput);
+        if (CLI.commandHistory.length > 21) // n+1 -- limit how back history goes
+            CLI.commandHistory.pop();
         var cmdStatus = doCommand(CLI.currentInput);
-            start();
+        start();
         //Prep terminal for new line
         if (cmdStatus == CLI.status.BAD_COMMAND)
             CLI.currentInput += "<br/>Unknown command";
-        // if (cmdStatus == "clear")
-        //     clear(CLI);
 
         CLI.STDIn = "";
         CLI.STDOut = "";
@@ -51,12 +59,11 @@ document.onkeypress = function (evt) {
         CLI.oldInput = CLI.oldInput + CLI.currentInput;
         CLI.currentInput = "";
 
-    } else if (evt.charCode != 60 && evt.charCode != 62){ // A character is typed (Not '<' or '>' for HTML reasons)
+    } else if (evt.charCode != 60 && evt.charCode != 62) { // A character is typed (Not '<' or '>' for HTML reasons)
         CLI.currentInput += String.fromCharCode(evt.which);
         container.innerHTML = CLI.oldInput + CLI.currentInput;
-
     }
-	window.scrollTo(0, document.body.scrollHeight); //Keep scrolling down
+    window.scrollTo(0, document.body.scrollHeight); //Keep scrolling down
 
 }
 
@@ -69,9 +76,31 @@ document.onkeydown = function (evt) {
             CLI.currentInput = CLI.currentInput.slice(0, CLI.currentInput.length - 1); //Remove a character
             container.innerHTML = CLI.oldInput + CLI.currentInput;
         }
+    } else if (evt.keyCode == 38) { //Up key history
+        evt.preventDefault();
+        if (CLI.commandPosition + 1 < CLI.commandHistory.length) {
+            CLI.commandPosition++;
+            CLI.currentInput = CLI.commandHistory[CLI.commandPosition];
+            container.innerHTML = CLI.oldInput + CLI.currentInput + CLI.cursor;
+        }
+    } else if (evt.keyCode == 40) { //Down key history
+        evt.preventDefault();
+        if (CLI.commandPosition - 1 > -1) {
+            CLI.commandPosition--;
+            CLI.currentInput = CLI.commandHistory[CLI.commandPosition];
+            container.innerHTML = CLI.oldInput + CLI.currentInput + CLI.cursor;
+        }
     }
 }
 
 function doCommand(input) {  //Commands are sent here to be parsed
     return Processes.listOfDevices['keyboard'].main(CLI.currentInput);
+}
+
+function cursor() {
+    if (CLI.cursor == "")
+        CLI.cursor = "_";
+    else
+        CLI.cursor = "";
+    container.innerHTML = CLI.oldInput + CLI.currentInput + CLI.cursor;
 }
