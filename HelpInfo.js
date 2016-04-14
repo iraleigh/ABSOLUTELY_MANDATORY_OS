0d4825b287b7753fffcb0038831a367fb4be9b16
Alex Brown <abrown@mail.sfsu.edu>
2016-04-14 03:41:40 -0700

    Man seems to be working. Need to add descriptions

--- HelpInfo.js
+++ HelpInfo.js
@@ -1,53 +1,27 @@
 var HelpInfo = {
-    //generateListOfProcesses: function () {
-    //    Processes.listOfProcesses.forEach(function (element, index, array) {
-    //        element.main.displayName = element.name;
-    //    });
-    //},
-    //findProcessByName: function (szProcessName) {
-    //    var oProcess;
-    //    Processes.listOfProcesses.forEach(function (element, index, array) {
-    //        if (element.name == szProcessName) {
-    //            oProcess = element;
-    //        }
-    //    });
-    //    return oProcess;
-    //},
+    //Path processes are OS functions like cat and ls, as opposed to RoutesCalculator
+    //When calling man by itself, for teaching the user how to use the OS, we only want OS processes.
+    getPathManuals: function(){
+        var pathMans = new Array();
+        for (getman_i = 0; getman_i < this.listOfHelp.length; getman_i++) {
+            if (this.listOfHelp[getman_i].path)
+                pathMans.push(this.listOfHelp[getman_i].name);
+        }
+        return pathMans;
+    },
     getManual: function (processName) {
-        //for (var getManualIterator = 0; getManualIterator < this.listOfHelp.length; getManualIterator++) {
-        //    if (this.listOfHelp[getManualIterator].name == processName)
-        //        return this.listOfHelp[getManualIterator].helpText;
-        //}
+        for (getman_i = 0; getman_i < this.listOfHelp.length; getman_i++) {
+            if (this.listOfHelp[getman_i].name == processName) {
+                return this.listOfHelp[getman_i].helpText;
+            }
+        }
         return "No manual found";
     },
     listOfHelp: []
 }
 
-function Manual(name, helpText, systemCommand) { // (string, string, bool)
+function Manual(name, helpText, path) { // (string, string, bool)
     this.name = name;
     this.helpText = helpText;
-    this.systemCommand = systemCommand;
+    this.path = path;
 }
-
-//function Process(name, main) {
-//    this.name = name;
-//    this.state = "Stop";
-//    this.program_counter = 0;
-//    this.var = {};
-//    this.main = main;
-//    this.threads = [];
-//    this.main.displayName = this.name;
-
-//    this.newThread = function (name, callback) {
-//        this.threads[name] = new Thread(this.name, name, this.var, callback);
-//    };
-//}
-
-//function Struct(name, value) {
-//    this.name = name;
-//    this.value = value;
-//}
-
-//arr[0] = new Struct("name1", "value1");
-
-//Processes.listOfProcesses.push(new Process("cat", cat));
--- bin/Man.js
+++ bin/Man.js
@@ -2,8 +2,16 @@ var man = function (counter) {
     switch (counter) {
         case 0:
             var args = this.args;
-            OS.display("test");
-            program_counter++;
+            //No args, show help
+            if (args[0] == null) {
+                var pathProcesses = HelpInfo.getPathManuals();
+                for (man_i = 0; man_i < pathProcesses.length; man_i++) {
+                    OS.display(pathProcesses[man_i]);
+                    OS.display(HelpInfo.getManual(pathProcesses[man_i]));
+                }
+            } else
+                OS.display(HelpInfo.getManual(args[0]));
+            this.program_counter++;
             break;
 
         default:
