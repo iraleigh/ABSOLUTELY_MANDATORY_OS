1187ccc3ccaf1f266d30f6de051c29b9e07e110e
iraleigh <iain.raleigh.dev@gmail.com>
2016-04-13 19:05:36 -0700

    Changed Scheduler to Oldest First algorithm
    The scheduler finds the last accessed Process that is in a Ready
    state.

---
+++ Scheduler.js
@@ -0,0 +1,36 @@
+OS.Scheduler = {
+	
+    runNextProcess: function () {
+
+        var oNextProcess;
+        var readyProcesses;
+
+        readyProcesses = Processes.listOfProcesses.filter(function (process) {
+            return process.state == "Ready";
+        });
+
+
+        var readyProcessExists = readyProcesses != undefined && readyProcesses.length > 0;
+
+        if (readyProcessExists) {
+            oNextProcess = readyProcesses.reduce(function(oldest_process, current_process){
+                return oldest_process.last_acess < current_process.last_acess ? current_process : oldest_process;
+            });
+        }
+
+
+
+        if (oNextProcess != undefined) {
+            oNextProcess.setLastAccess(Date.now());
+            oNextProcess.main(oNextProcess.program_counter);
+
+        } else {
+            return OS.Scheduler.runDevice();
+        }
+    },
+
+
+    runDevice: function () {
+        return Processes.listOfDevices['file_io'].main();
+    }
+}
