8ff84a1c4b4f86d26836e7509dd26a3dfecc630d
Alex Brown <abrown@mail.sfsu.edu>
2016-04-21 10:54:59 -0700

    Removed redundant init_d.js

--- init_d.js
+++
@@ -1,9 +0,0 @@
-function init_d() {
-    //OS
-    Processes.generateListOfProcesses();
-    addDummyFiles();
-    //CLI
-    CLI.oldInput = "<b>AMOS</b>\nFor help getting started, type 'help'\n\n\\> ";
-    CLI.commandHistory.push("");
-    window.document.getElementById('container').innerHTML = CLI.oldInput + CLI.cursor;
-}
