35b15a26721c034d27c9455fc797466e8897b0b9
Matt <mwishoff@yahoo.com>
2016-04-27 22:44:33 -0700

    I believe we may be having an issue with the hard drive.

--- bin/RemainingHDSpace.js
+++ bin/RemainingHDSpace.js
@@ -7,6 +7,11 @@ var RemainingHDSpace = function(counter)
     switch(counter)
     {
         case 0:
+            var currentSize = Directory.Files.reduce(flatten_callback, Directory.Files[0].accessLength()); //Calculate number of bytes taken up
+                currentSize = CAPACITY - currentSize; //CAPACITY is the size of the hard drive, this sets currentSize to the space left
+                //currentSize = (currentSize / 1000000); //Convert bytes to MB
+            OS.display(currentSize);
+            this.program_counter++;
             break;
 
         default:
