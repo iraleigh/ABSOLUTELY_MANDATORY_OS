11596862e368f17750665de6eedbbdabc148ee0b
Matt <mwishoff@yahoo.com>
2016-04-30 14:57:51 -0700

    Created the UserObject.js file.

---
+++ UserObject.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Matt on 4/30/2016.
+ */
+
+function user(name, pw)
+{
+    this.userName = name;
+    this.password = pw;
+
+    this.getUserName = function()
+    {
+        return this.userName;
+    };
+
+    this.setUserName = function(name)
+    {
+        this.userName = name;
+    };
+
+    this.getPassword = function()
+    {
+        return this.password;
+    };
+
+    this.setPassword = function(pw)
+    {
+        this.password = pw;
+    };
+
+}
