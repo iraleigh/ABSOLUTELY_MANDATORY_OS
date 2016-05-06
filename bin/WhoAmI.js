68ce9301b3f59ed4e5053bba4fa398084a1ff088
iraleigh <iain.raleigh.dev@gmail.com>
2016-05-05 23:14:23 -0700

    added whoami for usablity

---
+++ bin/WhoAmI.js
@@ -0,0 +1,7 @@
+var whoami = function (couter) {
+	OS.display(CurrentUserSingleton.getInstance().getUserName());
+	this.state = "Stop";
+}
+
+Processes.listOfProcesses.push(new Process("whoami", whoami));
+HelpInfo.listOfHelp.push(new Manual("whoami", "whoami", "Displays the current user."));
