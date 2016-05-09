d4cc84d454a5738feb607631c64d6c0a72108274
Matt <mwishoff@yahoo.com>
2016-05-09 16:07:21 -0700

    Small changes to what files are pushed into the groups directory.

--- GroupsFile.js
+++ GroupsFile.js
@@ -9,6 +9,7 @@ var groupsFile = function() {
             oTargetDir = Directory.Files[n];
         }
     }
-    oTargetDir.content.push(new File("Corinthians.txt", "Love is patient, love is kind. It does not envy, it does not boast, it is not proud. \nIt does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. \nLove does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres. \nLove never fails. But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away."));
-    oTargetDir.content.push(new File("Luke.txt", "But love your enemies, do good to them, and lend them without expecting to get anything back. \nThen your reward will be great, and you will be children of the Most High, because he is kind to the ungrateful and wicked."));
-}
+    //Push new users to the appropriate group files, to give them access.
+    oTargetDir.content.push(new File("newUser.txt", ""));  //New users will be put in here, and have access to ls, cat, Write, su, whoami, cd, touch, mkdir.
+    oTargetDir.content.push(new File("superGroup.txt", "")); //Will be granted access to everything.
+ }
